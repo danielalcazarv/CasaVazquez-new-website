7d75b69b7b767e8d950965af1b685b01b67dcc85
Daniel Alcazar <danielalcazar54@gmail.com>
2022-08-05 01:30:30 -0300

    ADD js MOD formulario contacto

---
+++ js/main.js
@@ -0,0 +1,5 @@
+//Variables
+const miFormulario = document.getElementById('formulario');
+const nombreCliente= document.getElementById('nombreCliente');
+const mailCliente= document.getElementById('mailCliente');
+const telCliente= document.getElementById('telCliente');
